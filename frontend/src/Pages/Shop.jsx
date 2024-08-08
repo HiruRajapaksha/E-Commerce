d05f9daa57eace61e2bf1556671700bdd1cf1eb0
Hirushan Rajapaksha <hirurajapaksha2002@gmail.com>
2024-08-09 03:40:49 +0530

    Frontend

---
+++ frontend/src/Pages/Shop.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Hero from '../Components/Hero/Hero';
+
+
+const shop = () => {
+  return (
+    <div>
+        <Hero />
+    </div>
+  )
+}
+
+export default shop
