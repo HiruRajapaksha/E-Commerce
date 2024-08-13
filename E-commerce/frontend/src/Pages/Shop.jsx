26bb265699c9ab38ecadf8196ce244319ded4c4c
Hirushan Rajapaksha <hirurajapaksha2002@gmail.com>
2024-08-13 16:52:00 +0530

    new update

--- E-commerce/frontend/src/Pages/Shop.jsx
+++ E-commerce/frontend/src/Pages/Shop.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import Hero from '../Components/Hero/Hero';
+import Popular from '../Components/Popular/Popular';
+import Offers from '../Components/Offers/Offers';
+import NewCollections from '../Components/NewCollections/NewCollections';
+import NewsLetter from '../Components/NewsLetter/NewsLetter';
+
 
 
 const shop = () => {
   return (
     <div>
         <Hero />
+        <Popular />
+        <Offers />  
+        <NewCollections />  
+        <NewsLetter /> 
+       
     </div>
   )
 }
 
-export default shop
+export default shop;
